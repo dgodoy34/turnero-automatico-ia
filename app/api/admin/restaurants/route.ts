0583e382ddde0334e1bb3289906122d0f738df9d
Diego Godoy <dgodoy34@gmail.com>
2026-03-16 20:38:05 -0300

    fix netlify runtime

--- app/api/admin/restaurants/route.ts
+++ app/api/admin/restaurants/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server"
 import { supabase } from "@/lib/supabaseClient"
 
+export const runtime = "nodejs"
+
 export async function GET(){
 
 const {data,error} = await supabase
