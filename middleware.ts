6d234016e3aad8f078f343f011072761f23939d5
Diego Godoy <dgodoy34@gmail.com>
2026-03-17 00:15:15 -0300

    fix middleware api

--- middleware.ts
+++ middleware.ts
@@ -19,6 +19,6 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
